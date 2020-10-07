e4909b0853edf2dba4d0cbcbccaa776e5211cb9b
dangjustintime <dangjustintime@gmail.com>
2020-10-07 14:01:01 -0700

    got server running

--- server.go
+++ server.go
@@ -2,11 +2,29 @@ package main
 
 import (
         "fmt"
+        "github.com/gorilla/mux"
+        "log"
+        "net/http"
+        "time"
 )
 
-const PORT = 8080
+const PORT = ":8080"
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+        w.WriteHeader(http.StatusOK)
+        fmt.Fprint(w, "Home Route")
+}
 
 func main() {
-        fmt.Println("server running...")
+        router := mux.NewRouter()
+        router.HandleFunc("/", HomeHandler)
+        server := &http.Server{
+                Handler: router,
+                Addr: PORT,
+                WriteTimeout: 10 * time.Second,
+                ReadTimeout: 10 * time.Second,
+        }
+
+        log.Fatal(server.ListenAndServe())
 }
 
